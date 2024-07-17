2df3f486105fc4133be6f65af03d970fa511058a
CHUMMY_CHAM(trueDOM) <69431340+ketchup0211@users.noreply.github.com>
2024-07-17 22:22:36 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int num = 0;
+    scanf("%d", &num);
+
+    for(int i = 0; i<num/4; i++){
+        printf("long ");
+    }
+    printf("int");
+    
+    return 0;
+}
