e91e672fd815b9bbb1edd2d5569314b5066eeed0
CHUMMY_CHAM(trueDOM) <69431340+ketchup0211@users.noreply.github.com>
2025-08-03 17:12:43 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int N;
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> N;
+  for(int i = 0; i < N ; i++){
+    for(int j = 0 ; j < i; j++) cout << ' ';
+    for(int k = 0; k < 2*(N-i)-1;k++) cout << '*';
+    cout << '\n';
+  }
+  
+  for(int i = N-2; i >= 0 ; i--){
+    for(int j = 0 ; j < i; j++) cout << ' ';
+    for(int k = 0; k < 2*(N-i)-1;k++) cout << '*';
+    cout << '\n';
+  }
+
+  return 0;
+}
